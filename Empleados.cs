6172f16a0265d4ed8ffce9469a5221ed434fc3d3
Guillermo Díaz Romero <guillermomigueltercero@gmail.com>
2024-05-27 01:26:54 -0300

    Tp terminado

--- Empleados.cs
+++ Empleados.cs
@@ -6,29 +6,60 @@ public enum Cargos{
     Administrativo,
     Ingeniero,
     Especialista,
-    Inverstigador
+    Investigador
 }
 public class Empleados{
     
-    
-    private string? nombre;
-    private string? apellido;
-    private char EstadoCivil;
-
-
+    private string? nombre; private string? apellido; private char EstadoCivil; private DateTime fechaNac; private Cargos cargo; private double SueldoBasico; private DateTime fechaIngreso;private int? antiguedadEmpleado; private int? edadEmpleado; private int? aniosParaJubilarse; private int? salarioTotal;
     public string? Nombre { get => nombre; set => nombre = value; }
     public string? Apellido { get => apellido; set => apellido = value; }
     public char EstadoCivil1 { get => EstadoCivil; set => EstadoCivil = value; }
     public double SueldoBasico1 { get => SueldoBasico; set => SueldoBasico = value; }
     public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
     public Cargos Cargo { get => cargo; set => cargo = value; }
+    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+    public int? AntiguedadEmpleado { get => antiguedadEmpleado; set => antiguedadEmpleado = value; }
+    public int? EdadEmpleado { get => edadEmpleado; set => edadEmpleado = value; }
+    public int? AniosParaJubilarse { get => aniosParaJubilarse; set => aniosParaJubilarse = value; }
+    public int? SalarioTotal { get => salarioTotal; set => salarioTotal = value; }
 
-    private DateTime fechaNac;
+    
+    public int calcularEdad(){
+        DateTime actual = DateTime.Now;
+        int edad = actual.Year - fechaNac.Year;
+        if (actual < fechaNac.AddYears(edad)){
+            edad--;
+        }
+        return edad;
+    }
 
-    private double SueldoBasico;
+    public int calcularAntiguedad(){
+        DateTime actual = DateTime.Now;
+        int antiguedad = actual.Year - fechaIngreso.Year;
+        return antiguedad;
+    }
 
-    private Cargos cargo;
+    public int jubilacion(int ed){
+        int jubila = 65 - ed;
+        return jubila;
+    }
 
+    public double calcularSalario(int anti){
+        double salario = SueldoBasico1;
+        if (anti < 20){
+            salario *= 1 + (anti/100);
+        }
+        else{
+            salario *= 1.25;
+        }
+        if (Cargo == Cargos.Ingeniero && Cargo == Cargos.Especialista){
+            salario *= 1.5;
+        }
+        if(EstadoCivil == 'c'){
+            salario += 150000;
+        }
 
-
+        Console.WriteLine("Su salario es: " + salario);
+        return salario;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ int num;
 
 do
 {
-    Console.WriteLine("Calculadora Reiterativa para el numero {0}",NuevaCalculadora.Resultado);
+    Console.WriteLine("Calculadora Reiterativa para el numero {0}", NuevaCalculadora.Resultado);
     Console.WriteLine("1.Sumar");
     Console.WriteLine("2.Restar");
     Console.WriteLine("3.Multiplicar");
@@ -90,11 +90,157 @@ static int numeroParaDividir()
 
 
 //Ejercicio 2
-Empleados Empleado = new Empleados();
 
-Empleado.Apellido = "probando";
-Empleado.Nombre = "pruebas";
-Empleado.EstadoCivil1 = 'c'; //casado
-Empleado.SueldoBasico1 = 300.000;
-Empleado.FechaNac = DateTime.Parse("2021-05-23");
-Empleado.
+Empleados[] Empleado = new Empleados[3];
+
+//Declaración de Variables
+string letra;
+char estado;
+int edad;
+string entrada;
+int opcion;
+double total = 0;
+float sal;
+int anti;
+int jubi;
+int mayor = 100;
+int persona = -99999;
+
+
+//Carga de datos de empleados
+for (int i = 0; i < 3; i++)
+{
+    Empleado[i] = new Empleados();
+    Console.WriteLine($"Ingrese los datos del {i+1}er empleado:");
+    //Apellido y Nombre
+    Console.Write("Apellido: ");
+    Empleado[i].Apellido = Console.ReadLine();
+    Console.Write("Nombre: ");
+    Empleado[i].Nombre = Console.ReadLine();
+    //Estado Civil
+    Console.WriteLine("Ingrese su estado civil");
+    do
+    {
+        Console.Write(" (c casado, s soltero): ");
+        letra = Console.ReadLine();
+        resultado = char.TryParse(letra, out estado);
+    } while (!resultado && (estado != 'c' || estado != 's'));
+    Empleado[i].EstadoCivil1 = estado;
+
+    //Salarios
+    do
+    {
+        Console.Write("\nIngrese su salario: ");
+        ingresado = Console.ReadLine();
+        resultado = float.TryParse(ingresado, out sal);
+    } while (!resultado || sal < 0);
+    Empleado[i].SueldoBasico1 = sal;
+
+    //Fechas
+    //Nacimiento
+    DateTime fechanac;
+    Console.WriteLine("Ingrese su Fecha de Nacimiento");
+    do
+    {
+        Console.Write("\n (Formato AAAA-MM-DD): ");
+        ingresado = Console.ReadLine();
+        resultado = DateTime.TryParse(ingresado, out fechanac);
+    } while (!resultado);
+    Empleado[i].FechaNac = fechanac;
+
+    //Fecha Ingreso
+    DateTime Ingreso;
+    Console.WriteLine("Ingrese su Fecha de Ingreso");
+    do
+    {
+        Console.Write("(Formato AAAA-MM-DD): ");
+        ingresado = Console.ReadLine();
+        resultado = DateTime.TryParse(ingresado, out Ingreso);
+    } while (!resultado);
+    Empleado[i].FechaIngreso = Ingreso;
+
+    //Cargo
+    Console.WriteLine("Ingrese su cargo:");
+    
+    do
+    {
+        Console.WriteLine("1.Auxiliar");
+        Console.WriteLine("2.Administrativo");
+        Console.WriteLine("3.Ingeniero");
+        Console.WriteLine("4.Especialista");
+        Console.WriteLine("5.Investigador");
+        entrada = Console.ReadLine();
+
+    } while (!int.TryParse(entrada, out opcion) && (opcion > 0 || opcion <= 5));
+    Empleado[i].Cargo = (Cargos)opcion;
+
+    //Calculando Edad-Antiguedad-Jubilacion-Salario
+    edad = Empleado[i].calcularEdad();
+    Empleado[i].EdadEmpleado = edad;
+    Console.WriteLine("Edad: " + edad);
+
+    anti = Empleado[i].calcularAntiguedad();
+    Empleado[i].AntiguedadEmpleado = anti;
+    Console.WriteLine("Antiguedad: " + anti);
+
+    jubi = Empleado[i].jubilacion(edad);
+    Empleado[i].AniosParaJubilarse = jubi;
+    if (jubi > 0)
+    {
+        Console.WriteLine($"Le quedan {jubi} años");
+    }
+    else
+    {
+        Console.WriteLine("Ya se puede jubilar");
+    }
+
+    total += Empleado[i].calcularSalario(anti);
+    if (jubi < mayor)
+    {
+        mayor = jubi;
+        persona = i;
+    }
+}
+
+Console.WriteLine($"Cantidad total en salarios a pagar: {total}");
+
+if(persona != -9999){
+    Console.Write("Datos de la persona más próxima a jubilarse:");
+    Console.Write("Apellido: ");
+    Console.Write(Empleado[persona].Apellido);
+    Console.Write("\n");
+    Console.Write("Nombre: ");
+    Console.Write(Empleado[persona].Nombre);
+    Console.Write("\n");
+    Console.Write("Edad: ");
+    Console.Write(Empleado[persona].EdadEmpleado);
+    Console.Write("\n");
+    Console.Write("Fecha de Nacimiento: ");
+    Console.Write(Empleado[persona].FechaNac);
+    Console.Write("\n");
+    Console.Write("Estado Civil: ");
+    Console.Write(Empleado[persona].EstadoCivil1);
+    Console.Write("\n");
+    Console.Write("Cargo que ocupa: ");
+    Console.Write(Empleado[persona].Cargo);
+    Console.Write("\n");
+    Console.Write("Fecha de ingreso al trabajo:");
+    Console.Write(Empleado[persona].FechaIngreso);
+    Console.Write("\n");
+    Console.Write("Antiguedad en el Empleo: ");
+    Console.Write(Empleado[persona].AntiguedadEmpleado);
+    Console.Write("\n");
+    Console.Write("Años para jubilarse: ");
+    Console.Write(Empleado[persona].AniosParaJubilarse);
+    Console.Write("\n");
+    Console.Write("Sueldo en bruto: ");
+    Console.Write(Empleado[persona].SueldoBasico1);
+    Console.Write("\n");
+    Console.Write("Salario TOTAL: ");
+    Console.Write(Empleado[persona].SalarioTotal);
+    Console.Write("\n");
+}
+else{
+    Console.WriteLine("Sus empleados estan todos para jubilarse");
+}
+
